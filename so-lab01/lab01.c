501cb5810b3f2864e288a126bc8485fa8839bf8b
maell-123 <robinson.contreras.27@unsch.edu.pe>
2026-09-15 07:52:55 -0500

    Add period to comment in Proceso struct

--- so-lab01/lab01.c
+++ so-lab01/lab01.c
@@ -2,7 +2,7 @@
 struct Proceso {
     int pid;
     char nombre[20];
-    int prioridad; // 1 (alta) a 5 (baja)
+    int prioridad; // 1 (alta) a 5 (baja).
 };
 
 int buscarMayorPrioridad(struct Proceso *procesos, int n) {
